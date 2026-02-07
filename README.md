2924abaf89aa7cb4d3d4aeea8196d36a9133b388
Anindya Ganguly <anindyaganguly19@gmail.com>
2026-02-07 18:27:48 +0530

    feat: initialize project with package.json, add date utility functions, and configure Vite

---
+++ README.md
@@ -0,0 +1,12 @@
+# cof7949fc8d07b8107226dc57
+
+Quick start:
+
+```
+$ npm install
+$ npm start
+````
+
+$ npm run dev
+
+use the above instructions to run the code
---
+++ index.js
@@ -0,0 +1,97 @@
+import { dates } from '/utils/dates.js'
+import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
+const tickersArr = []
+
+const generateReportBtn = document.querySelector('.generate-report-btn')
+
+generateReportBtn.addEventListener('click', fetchStockData)
+
+document.getElementById('ticker-input-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const tickerInput = document.getElementById('ticker-input')
+    if (tickerInput.value.length > 2) {
+        generateReportBtn.disabled = false
+        const newTickerStr = tickerInput.value
+        tickersArr.push(newTickerStr.toUpperCase())
+        tickerInput.value = ''
+        renderTickers()
+    } else {
+        const label = document.getElementsByTagName('label')[0]
+        label.style.color = 'red'
+        label.textContent = 'You must add at least one ticker. A ticker is a 3 letter or more code for a stock. E.g TSLA for Tesla.'
+    }
+})
+
+function renderTickers() {
+    const tickersDiv = document.querySelector('.ticker-choice-display')
+    tickersDiv.innerHTML = ''
+    tickersArr.forEach((ticker) => {
+        const newTickerSpan = document.createElement('span')
+        newTickerSpan.textContent = ticker
+        newTickerSpan.classList.add('ticker')
+        tickersDiv.appendChild(newTickerSpan)
+    })
+}
+
+const loadingArea = document.querySelector('.loading-panel')
+const apiMessage = document.getElementById('api-message')
+
+async function fetchStockData() {
+    document.querySelector('.action-panel').style.display = 'none'
+    loadingArea.style.display = 'flex'
+    try {
+        const stockData = await Promise.all(tickersArr.map(async (ticker) => {
+            const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.startDate}/${dates.endDate}?apiKey=${import.meta.env.VITE_POLYGON_API_KEY}`
+            const response = await fetch(url)
+            const data = await response.text()
+            const status = await response.status
+            if (status === 200) {
+                apiMessage.innerText = 'Creating report...'
+                return data
+            } else {
+                loadingArea.innerText = 'There was an error fetching stock data.'
+            }
+        }))
+        fetchReport(stockData.join(''))
+    } catch(err) {
+        loadingArea.innerText = 'There was an error fetching stock data.'
+        console.error('error: ', err)
+    }
+}
+
+async function fetchReport(data) {
+    /** AI goes here **/
+
+    const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY)
+
+    const model = genAI.getGenerativeModel({
+        model:"gemini-3-flash-preview",
+        systemInstruction:"You are a trading guru. Given data on share prices over the past three days, write a report of no more than 150 words describing the stock's performance and recommending whether to buy, hold or sell."
+    })
+
+    const prompt = data
+
+    try{
+        const result = await model.generateContent(prompt)
+        const response = await result.response
+        const text = response.text()
+
+        renderReport(text)
+    }
+    catch(err){
+        console.log(err)
+        loadingArea.innerText = 'Unable to access AI. Please refresh and try again later'
+    }
+
+}
+
+function renderReport(output) {
+    loadingArea.style.display = 'none'
+    const outputArea = document.querySelector('.output-panel')
+    const report = document.createElement('p')
+    outputArea.appendChild(report)
+    report.textContent = output
+    outputArea.style.display = 'flex'
+}
+
+//use npm run dev to run the index.js file

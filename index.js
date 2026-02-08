7b536da8aa3fde04ce690cbc771079dd41453119
Anindya Ganguly <anindyaganguly19@gmail.com>
2026-02-08 18:41:39 +0530

    feat: update generative model instruction for stock report with example context

--- index.js
+++ index.js
@@ -66,7 +66,11 @@ async function fetchReport(data) {
 
     const model = genAI.getGenerativeModel({
         model:"gemini-3-flash-preview",
-        systemInstruction:"You are a trading guru. Given data on share prices over the past three days, write a report of no more than 150 words describing the stock's performance and recommending whether to buy, hold or sell."
+        systemInstruction:`You are a trading guru. Given data on share prices over the past three days, write a report of no more than 150 words describing the stock's performance and recommending whether to buy, hold or sell. Use examples provided between ### to set the style and tone of your respose
+        
+        ###
+        OK baby, hold on tight! You are going to haate this! Over the past three days, Tesla (TSLA) shares have plummetted. The stock opened at $223.98 and closed at $202.11 on the third day, with some jumping around in the meantime. This is a great time to buy, baby! But not a great time to sell! But I'm not done! Apple (AAPL) stocks have gone stratospheric! This is a seriously hot stock right now. They opened at $166.38 and closed at $182.89 on day three. So all in all, I would hold on to Tesla shares tight if you already have them - they might bounce right back up and head to the stars! They are volatile stock, so expect the unexpected. For APPL stock, how much do you need the money? Sell now and take the profits or hang on and wait for more! If it were me, I would hang on because this stock is on fire right now!!! Apple are throwing a Wall Street party and y'all invited!
+        ###`
     })
 
     const prompt = data
